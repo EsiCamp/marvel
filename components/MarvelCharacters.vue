b3c29aec215759c41e6366f1a59c8f65a27432ab
Ehsan Tashakkori <ehsan.catalan@gmail.com>
2025-03-11 20:52:47 +0330

    fix: pagination list

--- components/MarvelCharacters.vue
+++ components/MarvelCharacters.vue
@@ -6,6 +6,7 @@ const currentPage = ref(0);
 const pageSize = ref(12);
 const searchQuery = ref("");
 const searchTimeout = ref(null);
+const totalItems = ref(0);
 
 const fetchCharactersData = async () => {
   const offset = currentPage.value * pageSize.value;
@@ -30,6 +31,16 @@ const {
   watch: [currentPage, searchQuery],
 });
 
+watch(
+  charactersResponse,
+  (newValue) => {
+    if (newValue && newValue.data && newValue.data.total !== undefined) {
+      totalItems.value = newValue.data.total;
+    }
+  },
+  { immediate: true }
+);
+
 const characters = computed(() => {
   if (
     charactersResponse.value &&
@@ -42,12 +53,65 @@ const characters = computed(() => {
   return [];
 });
 
-const hasMorePages = computed(() => {
-  return characters.value.length >= pageSize.value;
+const totalPages = computed(() => {
+  return Math.ceil(totalItems.value / pageSize.value);
+});
+
+const paginationRange = computed(() => {
+  const totalPageCount = totalPages.value;
+  const currentPageNum = currentPage.value + 1;
+
+  const siblingCount = 1;
+  const totalPageNumbers = siblingCount * 2 + 3;
+
+  if (totalPageCount <= totalPageNumbers) {
+    return Array.from({ length: totalPageCount }, (_, i) => i + 1);
+  }
+
+  const leftSiblingIndex = Math.max(currentPageNum - siblingCount, 1);
+  const rightSiblingIndex = Math.min(
+    currentPageNum + siblingCount,
+    totalPageCount
+  );
+
+  const shouldShowLeftDots = leftSiblingIndex > 2;
+  const shouldShowRightDots = rightSiblingIndex < totalPageCount - 1;
+
+  const firstPageIndex = 1;
+  const lastPageIndex = totalPageCount;
+
+  if (!shouldShowLeftDots && shouldShowRightDots) {
+    const leftItemCount = 3 + 2 * siblingCount;
+    const leftRange = Array.from({ length: leftItemCount }, (_, i) => i + 1);
+    return [...leftRange, "RIGHT_DOTS", lastPageIndex];
+  }
+
+  if (shouldShowLeftDots && !shouldShowRightDots) {
+    const rightItemCount = 3 + 2 * siblingCount;
+    const rightRange = Array.from(
+      { length: rightItemCount },
+      (_, i) => totalPageCount - rightItemCount + i + 1
+    );
+    return [firstPageIndex, "LEFT_DOTS", ...rightRange];
+  }
+
+  if (shouldShowLeftDots && shouldShowRightDots) {
+    const middleRange = Array.from(
+      { length: rightSiblingIndex - leftSiblingIndex + 1 },
+      (_, i) => leftSiblingIndex + i
+    );
+    return [
+      firstPageIndex,
+      "LEFT_DOTS",
+      ...middleRange,
+      "RIGHT_DOTS",
+      lastPageIndex,
+    ];
+  }
 });
 
 const changePage = (newPage) => {
-  if (newPage >= 0) {
+  if (newPage >= 0 && newPage < totalPages.value) {
     currentPage.value = newPage;
   }
 };
@@ -63,7 +127,7 @@ const debounceSearch = () => {
 </script>
 
 <template>
-  <div class="marvel-characters">
+  <div>
     <div class="search-container">
       <input
         v-model="searchQuery"
@@ -109,22 +173,63 @@ const debounceSearch = () => {
       </div>
     </div>
 
-    <!-- Pagination -->
-    <div class="pagination">
+    <div
+      v-if="totalPages > 0"
+      class="pt-6 pb-8 flex items-center justify-center gap-x-2"
+    >
       <button
+        @click="changePage(0)"
         :disabled="currentPage === 0"
-        @click="changePage(currentPage - 1)"
-        class="pagination-button"
+        class="w-[24px] text-white disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
+        aria-label="First page"
       >
-        Previous
+        <<
       </button>
-      <span>Page {{ currentPage + 1 }}</span>
       <button
-        :disabled="!hasMorePages"
-        @click="changePage(currentPage + 1)"
-        class="pagination-button"
+        @click="changePage(currentPage - 1)"
+        :disabled="currentPage === 0"
+        class="w-[24px] text-white disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
+        aria-label="Previous page"
       >
-        Next
+        <
+      </button>
+
+      <template v-for="(pageNumber, index) in paginationRange" :key="index">
+        <template
+          v-if="pageNumber === 'LEFT_DOTS' || pageNumber === 'RIGHT_DOTS'"
+        >
+          <span class="text-[#989A9C] text-base font-medium">...</span>
+        </template>
+        <template v-else>
+          <button
+            @click="changePage(pageNumber - 1)"
+            :class="[
+              'rounded-full w-[24px] flex items-center justify-center text-base cursor-pointer',
+              currentPage === pageNumber - 1
+                ? 'bg-[#DC2626] text-white'
+                : 'text-[#989A9C]',
+            ]"
+          >
+            {{ pageNumber }}
+          </button>
+        </template>
+      </template>
+
+      <button
+        @click="changePage(currentPage + 1)"
+        :disabled="currentPage >= totalPages - 1"
+        class="w-[24px] text-white disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
+        aria-label="Next page"
+      >
+        >
+      </button>
+      <button
+        @click="changePage(totalPages - 1)"
+        :disabled="currentPage >= totalPages - 1"
+        class="w-[24px] text-white disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
+        aria-label="Last page"
+      >
+        >>
       </button>
     </div>
   </div>
